8bb8ff61b777c0860926f49c6d2228000ff928e7
Goofy <piyush.dongre16@gmail.com>
2020-06-11 19:26:11 +0530

    Added API_view with get and post

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from profiles_api import serializers
+
+
+class HelloApiView(APIView):
+    """Test API View"""
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format= None):
+        """request a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over you application login',
+            'is mapped manually to URLS'
+        ]
+
+        return Response({'message': 'Hello', 'an_apiview' : an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+            serializer.errors,
+            status = status.HTTP_400_BAD_REQUEST
+            )
