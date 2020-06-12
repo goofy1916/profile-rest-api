d6b9c5164675b798b2b1a9b4ef9552d8a17c7e99
Goofy <piyush.dongre16@gmail.com>
2020-06-12 13:05:06 +0530

    ViewSets implied

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serializers
 
@@ -33,3 +34,43 @@ class HelloApiView(APIView):
             serializer.errors,
             status = status.HTTP_400_BAD_REQUEST
             )
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    serializer_class = serializers.HelloSerializer
+
+
+    def list(self, request):
+        """Return a hello message"""
+        an_viewset = [
+            'Uses HTTP methods as function (get, post, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over you application login',
+            'is mapped manually to URLS'
+        ]
+
+        return Response({'message':'Hellow World', 'viewset': an_viewset})
+
+    def create(self, request):
+        """Create a new hello message"""
+        serializer = self.serializer_class(data = request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            serializer.errors
+            status = status.HTTP_400_BAD_REQUEST
+
+
+    def  retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+        return Response({'http_method': 'GET'})
+
+
+    def  update(self, request, pk=None):
+        """Handle updating an object by its ID"""
+        return Response({'http_method': 'UPDATE'})
